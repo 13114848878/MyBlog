6b3a2c984b70d6bfb53013a758e3cf6602e797c0
TianSen <414319563@qq.com>
2019-10-07 22:16:02 +0800

    解决了代码不高亮的问题，在详情页增加了左右导航栏，在列表也增加了分页功能，将页面进行了调整“

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from blog.models import Article, Category
-from markdown import markdown
+from markdown import markdown, Markdown
 from django.template.loader import get_template
+from django.core.paginator import Paginator
 # Create your views here.
 
 # def index(request):
@@ -29,19 +31,38 @@ def list(request):
 	# print(allarticle)
 	# allarticle.body = markdown.markdown(allarticle.body)
 	#把查询出来的分类封装到上下文里
-	context = {'articles': allarticle,}
-	return render(request, 'blog/list.html', context)#把上下文传到index.html页面
+	
+	# 设置每页显示几篇文章
+	paginator = Paginator(allarticle, 2)
+
+	# page_range is like range(1,2)
+	page_range = paginator.page_range
+	
+	# a = [i for i in page_range]
+	
+	# 获取 url 中的页码
+	page = request.GET.get('page')
+	# print(page)
+	#
+	# 将导航对象相应的页码内容返回给 articles, 当page不是数字的时候返回第一页
+	articles = paginator.get_page(page)
+
+	context = {'articles': articles, "page_range":page_range}
+	return render(request, 'blog/list.html', context)
 
 
 #内容页
 def show(request, sid):
-	article = Article.objects.get(id=sid)
-	article.body = markdown(article.body.replace("\r\n", '  \n'),
-		extensions=['markdown.extensions.extra',
-		'markdown.extensions.codehilite',
-		'markdown.extensions.toc',],
-		safe_mode=True,enable_attributes=False)
-	context = {'article': article,}
+	article = get_object_or_404(Article, pk=sid)
+	# article = Article.objects.get(id=sid)
+	# 此处使用了Markdown替换了之前的markdown 
+	md = Markdown(extensions=['markdown.extensions.extra',
+	 	'markdown.extensions.codehilite',
+	 	'markdown.extensions.toc',],)
+
+	article.body = md.convert(article.body.replace("\r\n", '  \n'),)
+
+	context = {'article': article, "toc":md.toc}
 	# template = get_template('blog/show.html')
 	# return_dict = {'title': article.title, 'category': article.category, 
 	# 'user':article.user, 'date': article.created_time, 
